89bc985499f728c509c883b3fa34a5e67a0aaa9a
Yy <1156388293@aliyun.com>
2022-11-24 15:01:07 +0800

    hot-fix commit2

--- src/main/java/com/gtguigu/GitTest.java
+++ src/main/java/com/gtguigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("Git 3");
         System.out.println("Git 4");
         System.out.println("Git 5");
+        System.out.println("hot-fix 6");
     }
 }
